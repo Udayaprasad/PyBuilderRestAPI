ba549f6b1fe557e80936a7e3bbc92e256f29f08b
Udayaprasad <Udayaprasad@users.noreply.github.com>
2019-11-30 12:14:40 +0530

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -rf PyBuilderRestAPI
 kill -9 $(lsof -t -i:5000)
-pip install --upgrade pip
 echo "\n ############# Start Git Cloning Operation ############# \n"
 git clone https://github.com/Udayaprasad/PyBuilderRestAPI.git
 echo "\n ############# Change Directory to PyBuilderRestAPI ############# \n"
